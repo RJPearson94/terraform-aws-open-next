7e756ffe7d6f855beec4adcb39cc54f82fe1f1dd
RJPearson94 <RJPearson94@users.noreply.github.com>
2023-12-05 20:43:58 +0000

    fix: correct line endings on invalidate-cloudfront script
    Resolves #12

--- modules/legacy/scripts/invalidate-cloudfront.sh
+++ modules/legacy/scripts/invalidate-cloudfront.sh
@@ -4,9 +4,9 @@ INVALIDATION_ID=$(aws cloudfront create-invalidation --distribution-id $CDN_ID -
 wait_for_invalidation() {
     while [ $(aws cloudfront get-invalidation --id $INVALIDATION_ID --distribution-id $CDN_ID | jq -r '.Invalidation.Status') != "Completed" ]
     do
-        aws cloudfront wait invalidation-completed --distribution-id $CDN_ID --id $INVALIDATION_ID                       
+        aws cloudfront wait invalidation-completed --distribution-id $CDN_ID --id $INVALIDATION_ID
     done
-        echo "Invalidation Cache Complete";  
+        echo "Invalidation Cache Complete";
 }
 wait_for_invalidation
 aws cloudfront wait distribution-deployed --id $CDN_ID
--- modules/tf-aws-open-next-public-resources/scripts/promote-distribution.sh
+++ modules/tf-aws-open-next-public-resources/scripts/promote-distribution.sh
@@ -19,4 +19,3 @@ while true; do
 
     sleep $INTERVAL
 done
-
